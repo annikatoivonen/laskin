b66d360ec09646a633651c900fac7997f926f4d6
annikatoivonen <annika.97@windowslive.com>
2022-09-04 18:34:35 +0300

    historia lisätty

--- App.js
+++ App.js
@@ -1,4 +1,4 @@
-import { StyleSheet, TextInput, View, Button, Text } from 'react-native';
+import { StyleSheet, TextInput, View, Button, Text, FlatList } from 'react-native';
 import React from 'react';
 
 export default function App() {
@@ -6,14 +6,68 @@ export default function App() {
   const [eka, setEka] = React.useState(0);
   const [toka, setToka] = React.useState(0);
   const [tulos, setTulos] = React.useState(0);
+  const [data, setData] = React.useState([]);
+  const [text, setText] = React.useState('');
 
   const plus = () => {
-    setTulos(Number(eka) + Number(toka))
+    const newTulos = (Number(eka) + (Number(toka)));
+    setTulos(newTulos);
+    setText(eka + " + " + toka + " = " + newTulos);
+    const newData = [...data, {key: text}];
+    setData(newData);
   };
 
   const miinus = () => {
-    setTulos(eka - toka)
+    const uusiTulos = (Number(eka) - (Number(toka)));
+    setTulos(uusiTulos);
+    setText(eka + " - " + toka + " = " + uusiTulos);
+    const newData = [...data, {key: text}];
+    setData(newData);
   };
+
+  return (
+    <View style={styles.container}>
+      <View style={{flex:4, justifyContent:'center', alignItems:'center'}}
+      >
+      <Text style={styles.text}>Result: {tulos}</Text>
+
+        <TextInput 
+        style={styles.input}
+        keyboardType="numeric"
+        onChangeText={setEka}
+        value={eka}>
+        </TextInput>
+
+        <TextInput 
+        style={styles.input}
+        keyboardType="numeric"
+        onChangeText={setToka}
+        value={toka}>
+        </TextInput>
+        </View>
+        <View style={styles.button}>
+          <Button 
+          onPress={plus}
+          title="+">
+          </Button>
+
+          <Button
+          title="-"
+          onPress={miinus}>
+          </Button>
+          </View>
+          <View style={{flex:5}}>
+         <Text style={styles.text}>History:</Text>
+            <FlatList
+            data={data}
+            renderItem={({ item }) =>
+            <Text>{item.key}</Text>
+            }>
+            </FlatList>
+           </View>
+    </View>
+  ); 
+}
   const styles = StyleSheet.create({
     container: {
       flex: 1,
@@ -32,48 +86,15 @@ export default function App() {
     },
   
     button: {
+      flex:1,
       flexDirection: 'row',
       justifyContent: 'space-between',
-      marginHorizontal: 60,
       borderBottomColor: 'blue',
     },
 
     text: {
       fontSize: 20,
     },
+
   });
 
-  return (
-    <View style={styles.container}>
-      
-      <Text style={styles.text}>Result: {tulos}
-      </Text>
-      <View>
-        <TextInput 
-        style={styles.input}
-        keyboardType="numeric"
-        onChangeText={setEka}
-        value={eka}>
-        </TextInput>
-        <TextInput 
-        style={styles.input}
-        
-        keyboardType="numeric"
-        onChangeText={setToka}
-        value={toka}>
-        </TextInput>
-        <View style={styles.button}>
-          <Button 
-          onPress={plus}
-          title="+">
-          </Button>
-
-          <Button
-          title="-"
-          onPress={miinus}>
-          </Button>
-        </View>
-      </View>
-    </View>
-  );
-}
